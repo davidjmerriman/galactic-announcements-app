7ad65dedc293364d32f224c58ef76ec4e237a276
David Merriman <merriman.david.j@gmail.com>
2024-05-08 13:40:20 -0500

    Adapt to API change to remove extra data level, add error handling

--- src/Announcements.js
+++ src/Announcements.js
@@ -8,14 +8,19 @@ function Announcements() {
     const [announcements, setAnnouncements] = useState([]);
     const [hasMore, setHasMore] = useState(true);
     const [offset, setOffset] = useState(pageSize);
+    const [error, setError] = useState(null);
   
     useEffect(() => {
       axios
         .get(`http://localhost:8000/api/announcements?offset=0&limit=${pageSize}`)
         .then(response => {
           setOffset(pageSize);
-          setAnnouncements([...announcements, ...response.data.data.announcements]);
+          setAnnouncements([...announcements, ...response.data.announcements]);
           setHasMore(true);
+        })
+        .catch(function(error) {
+          console.log(error);
+          setError(error);
         });
     }, []);
   
@@ -24,9 +29,24 @@ function Announcements() {
         .get(`http://localhost:8000/api/announcements?offset=${offset}&limit=${pageSize}`)
         .then(response => {
           setOffset(offset + pageSize);
-          setAnnouncements([...announcements, ...response.data.data.announcements]);
-          setHasMore(announcements.length < response.data.data.totalCount);
+          setAnnouncements([...announcements, ...response.data.announcements]);
+          setHasMore(announcements.length < response.data.totalCount);
+        })
+        .catch(function(error) {
+          console.log(error);
+          setError(error);
         });
+    }
+
+    if (error) {
+      return <div className="announcements-container">
+          <div className="error-container">
+            <h2>I felt a great disturbance in the Network, as if millions of announcements suddenly cried out in terror and were suddenly silenced.</h2>
+            <h2>I fear something terrible has happened.</h2>
+            <hr/>
+            <p>...or you may need to start the API service first, and refresh. :)</p>
+          </div>
+        </div>
     }
 
     return (
